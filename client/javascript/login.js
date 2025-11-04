7cb105fec44a2fec4d0f59d496b2cc0330b57a98
jlynch24 <jlynch24@my.brookdalecc.edu>
2025-11-03 20:19:28 -0500

    Started on
    St

---
+++ client/javascript/login.js
@@ -0,0 +1,24 @@
+//This page will log the information taken from signup.html into the database 
+// (when I can connect the database)
+
+//Add a listener so that when the user clicks the submit button it dose something.
+document.getElementById("registerForm").addEventListener("submit", async (e) => {
+    
+    const collectedData = {
+        email: document.getElementById("email").value,
+        username: document.getElementById("username").value,
+        password: document.getElementById("password").value
+    };
+
+    // Send it to the server with post as a JSON object.
+    const response = await fetch("/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify(data)
+    });
+
+    //After sign in, send the user to login.
+    if(response.ok) {
+        window.location.href = "client\genPages\signup.html";
+    }
+});
--- server/router.js
+++ server/router.js
@@ -5,25 +5,32 @@ var router =function(app) {
     app.get('/', function(req, res) {
         res.sendFile(path.join(__dirname + "/../client/genPages/index.html"))
     });
-};
-var router =function(app) { //Rules page
-    app.get('/', function(req, res) {
+
+//Rules page
+    app.get('/rules', function(req, res) {
         res.sendFile(path.join(__dirname + "/../client/genPages/rules.html"))
     });
-};
-var router =function(app) { //Lore page
-    app.get('/', function(req, res) {
+//Lore page
+    app.get('/lore', function(req, res) {
         res.sendFile(path.join(__dirname + "/../client/genPages/lore.html"))
     });
-};
-var router =function(app) { //Pufferfish page
-    app.get('/', function(req, res) {
+
+//Pufferfish page
+    app.get('/pufferfish', function(req, res) {
         res.sendFile(path.join(__dirname + "/../client/genPages/pufferfish.html"))
     });
-};
-var router =function(app) { //Character Creation Menu PAge
-    app.get('/', function(req, res) {
+//Character Creation Menu PAge
+    app.get('/ccmenu', function(req, res) {
         res.sendFile(path.join(__dirname + "/../client/genPages/ccmenu.html"))
+    });
+
+//Sign Up
+    app.get('/signup', function(req, res) {
+        res.sendFile(path.join(__dirname + "/../client/genPages/signup.html"))
+    });
+    //Log In
+    app.get('/login', function(req, res) {
+        res.sendFile(path.join(__dirname + "/../client/genPages/login.html"))
     });
 };
 
