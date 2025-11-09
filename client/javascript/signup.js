cf0827d7ae8a811d3d3c46529a2fec89043d50f7
jlynch24 <jlynch24@my.brookdalecc.edu>
2025-11-08 22:22:39 -0500

    Minor Page Updates + Registration Progress
    Did some minor fleshing out with pages for the character sheet as well as worked on signup.js. I still haven't managed to connect it to the server and I spend some time trying to understand the process of Fetches. I'd give it another shot tomorrow after some rest.

--- client/javascript/signup.js
+++ client/javascript/signup.js
@@ -4,14 +4,27 @@
 //Add a listener so that when the user clicks the submit button it dose something.
 document.getElementById("registerForm").addEventListener("submit", async (e) => {
     
+    //Collected the data and prepare it to be shipped
     const collectedData = {
         email: document.getElementById("email").value,
         username: document.getElementById("username").value,
         password: document.getElementById("password").value
     };
 
+    //Link servers! Link!
     // Send it to the server.
-    fetch
+    fetch("http://localhost:5001/signup", { //Fetch the data from the page.
+        //Make it post, link it to server, pack into a json object
+        method: "POST", //Make this a POST fetch
+        headers: "application/json",    
+        body: JSON.stringify(collectedData),
+    })
+    .then(data => {
+        if(!response.ok) {
+            throw new Error("signup.js is not working!")
+        }
+            return response.json();
+    })
 
     //After sign in, send the user to login.
     if(response.ok) {
