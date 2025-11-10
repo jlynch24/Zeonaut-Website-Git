23e702a8c6039139472dc92d3efdf453bcd10c53
jlynch24 <jlynch24@my.brookdalecc.edu>
2025-11-10 17:47:55 -0500

    Login.js progress
    Worked on login.js to get in in shape, at least for progress. Going to do some adjustments tomorrow to see if I can get it in shape, at least before wednesday.

--- client/javascript/login.js
+++ client/javascript/login.js
@@ -1,15 +1,29 @@
+//Login backend.
 //When the button is pressed
 document.getElementById("login").addEventListener("submit", async (e) => {
 
-    //Get the values user inputted.
+    //Get the values user inputted, like with signup but we only care about two of them.
     const collectedData = {
         email: document.getElementById("email").value,
         password: document.getElementById("password").value
     };
 
-    //After log in, send the user to main page, hopefully now signed in.
-    if(response.ok) {
-        window.location.href = "client\genPages\index.html";
-    }
-
+    //Sends the data to server.
+    fetch("https://localhost:5001/login", {
+        method: "POST", //Going with post because we still need to collect the user data, at least to compare it.
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(collectedData)
+    })
+    .then(response => {     //If it's not working explicitly then 
+        if (!response.ok) {
+            throw new Error("login.js is not working!")
+        }
+    })
+    .then(data => {     //Sends the user back to index.html logged in, but only if they answer correctly.
+        if (data) {
+            window.location.href = "index.html";
+        } else {
+            alert("Wrong email and password, try again or");
+        }
+    })
 });
